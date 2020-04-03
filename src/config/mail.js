9dbd2ae340d2512a4daae959694e86725f610fc0
wescleysudre <wescley.sudre@sage.com>
2020-04-03 16:42:18 -0300

    Email e template de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '293635520c7490',
+    pass: 'c56702edf79a52',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
